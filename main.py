d267c738dce55f6e05f37130afcbd9e1d12be92f
galangrakasaputra <104204878+galangrakasaputra@users.noreply.github.com>
2025-06-30 22:01:21 +0700

    memilih pilihan

---
+++ main.py
@@ -0,0 +1,18 @@
+print("\nMenu")
+print("1. Melihat Kontak")
+print("2. Menambah Kontak")
+print("3. Menghapus Kontak")
+print("4. Selesai")
+
+pilihan = input("Pilih Menu (Angka saja) : ")
+
+if pilihan == 1:
+    pass
+elif pilihan == 2:
+    pass
+elif pilihan == 3:
+    pass
+elif pilihan == 4:
+    pass
+else:
+    print("Pilihan tidak ada")
