b1aa4529a15eedb697ee1b0d0cc9a2ba9bdf8f71
galangrakasaputra <104204878+galangrakasaputra@users.noreply.github.com>
2025-08-03 14:26:32 +0700

    menambah fungsi CRUD

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-print("\nMenu")
-print("1. Melihat Kontak")
-print("2. Menambah Kontak")
-print("3. Menghapus Kontak")
-print("4. Selesai")
+test = {'Nama' : "Test", 'HP' : '2938123'}
 
-pilihan = input("Pilih Menu (Angka saja) : ")
+kontak = [test]
+while True:
+    print("\nMenu")
+    print("1. Melihat Kontak")
+    print("2. Menambah Kontak")
+    print("3. Menghapus Kontak")
+    print("5. Selesai")
 
-if pilihan == 1:
-    pass
-elif pilihan == 2:
-    pass
-elif pilihan == 3:
-    pass
-elif pilihan == 4:
-    pass
-else:
-    print("Pilihan tidak ada")
+    pilihan = int(input("Pilih Menu : "))
+
+    if pilihan == 1:
+        if kontak:
+            for num, item in enumerate(kontak, start=1):
+                print(f'\n{num}. {item["Nama"]} ({item["HP"]})')
+        else:
+            print("tidak ada kontak")
+    elif pilihan == 2:
+        nama = input("Masukan Nama Baru : ")
+        hp = int(input("Masukan Nomor HP baru : " ))
+        kontak_baru = {'Nama': nama, 'HP': hp}
+        kontak.append(kontak_baru)
+        print("berhasil di tambah")
+    elif pilihan == 3:
+        if kontak:
+            for num, item in enumerate(kontak, start=1):
+                print(f'{num}. {item["Nama"]} ({item["HP"]})')    
+        else:
+            print("tidak ada kontak")
+            continue
+        
+        hapus = int(input("Kontak mana yang ingin Dihapus ? "))
+        del kontak[hapus-1]
+        print("berhasil dihapus")
+    elif pilihan == 4:
+        break
+    else:
+        print("Pilihan tidak ada")
